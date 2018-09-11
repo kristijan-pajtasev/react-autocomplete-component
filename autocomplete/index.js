12fb16b0f2d2b4af3a8331dbf535d5645bfff8a7
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-09-11 20:20:35 +0100

    init react component

---
+++ autocomplete/index.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+class Autocomplete extends  PureComponent {
+    render() {
+        return (<div>Autocomplete</div>)
+    }
+}
+
+export default Autocomplete;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Autocomplete from './autocomplete';
+
+ReactDOM.render(<Autocomplete/>, document.getElementById("react-container"));
